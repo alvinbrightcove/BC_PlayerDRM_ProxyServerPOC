32c3883075e16a80783e711a7b5937ba4cd1070c
muhammedafeef <57178328+muhammedafeef@users.noreply.github.com>
2020-03-19 11:21:53 +0530

    updated with neustar implementation

--- Proxy Server/midstreamcheck.js
+++ Proxy Server/midstreamcheck.js
@@ -1,30 +1,9 @@
 const https = require('https');
-let restapi = require('./restapi')
-const drm_proxy = require('./drm_proxy')
+let restapi = require('./restapi');
 const promise = require('promise');
-let bc_tokengenerator = require('./bc_tokengenerator')
+let bc_tokengenerator = require('./bc_tokengenerator');
 const BC_PLAYBACKRIGHTS_BASEURL = 'https://playback-rights.api.brightcove.com/v1/accounts/';
 const BC_CMSAPI_BASEURL = 'https://cms.api.brightcove.com/v1/accounts/';
-const IPLOC_FINDER_URL = 'https://tools.keycdn.com/geo.json?host=';
-
-let getCurrentLocation = (ipaddress) => {
-    return new promise((resolve, reject) => {
-        method = 'GET'
-        url = IPLOC_FINDER_URL + ipaddress
-        headers = ''
-        body = ''
-        restapi.callRestAPI(method, url, headers, body).then(response => {
-            var currentzip
-            var loc = JSON.parse(response.body);
-            currentzip = loc.data.geo['postal_code']
-            // console.log("\nCurrent zip code: " + currentzip+"\n");
-            return resolve(currentzip);
-        }).catch(error => {
-            return reject("Zip not resolved");
-        })
-
-    })
-}
 
 let getPlaybackRights = (videoId, accountId) => {
     return new promise((resolve, reject) => {
@@ -42,14 +21,12 @@ let getPlaybackRights = (videoId, accountId) => {
                     resp = JSON.parse(response.body)
                     allowedzip = []
                     allowedzip = allowedzip.concat(resp.geo.allowed_zip_codes)
-                    allowedzip = ['US-123456', 'US-456789', 'US-20149', 'US-987654']
-                    // console.log("Allowed zip codes: " + allowedzip)
+                    // allowedzip = ['US-123456', 'US-456789', 'US-20147', 'US-987654']
                     allowedzip_striped = []
                     for (zip of allowedzip) {
                         stripedzip = zip.replace('US-', '')
                         allowedzip_striped.push(stripedzip)
                     }
-                    // console.log("allowedzip_striped = " + allowedzip_striped+"\n")
                     return resolve(allowedzip_striped);
                 }).catch(error => {
                     return reject("Playback rights access failed");
@@ -73,7 +50,6 @@ let getPlaybackRightsId = (videoId, accountId, accesstoken) =>{
             var playbackrightId = resp.playback_rights_id
             var assetId = resp.reference_id
             module.exports.assetId = resp.reference_id
-            // console.log("Playback right ID: "+playbackrightId+"\nAsset ID: "+assetId+"\n")
             return resolve(playbackrightId)
         }).catch(error =>{
             return reject("Playback rights id request failed");
@@ -95,4 +71,4 @@ let midstreamCheck = (currentzip, allowedzip) => {
     })
 }
 
-module.exports = { getCurrentLocation, getPlaybackRights, midstreamCheck}
+module.exports = {getPlaybackRights, midstreamCheck}
