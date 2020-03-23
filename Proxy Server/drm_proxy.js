ef304944274b43d0776ffbe29d6e3cc044382260
BharanidaranSivakumar <57176728+BharanidaranSivakumar@users.noreply.github.com>
2020-03-23 18:46:14 +0530

    Update drm_proxy.js

--- Proxy Server/drm_proxy.js
+++ Proxy Server/drm_proxy.js
@@ -25,40 +25,12 @@ var httpServer = http.createServer(app);
 var httpsServer = https.createServer(options, app);
 
 function decode(base64) {
-    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-    // Use a lookup table to find the index.
-    var lookup = new Uint8Array(256);
-    for (var i = 0; i < chars.length; i++) {
-        lookup[chars.charCodeAt(i)] = i;
+    try {
+        return Buffer.from(str, "base64");
+    } catch (err) {
+        log.Warning('Body decode error: ', err);
     }
-
-    var bufferLength = base64.length * 0.75,
-        len = base64.length, i, p = 0,
-        encoded1, encoded2, encoded3, encoded4;
-
-    if (base64[base64.length - 1] === "=") {
-        bufferLength--;
-        if (base64[base64.length - 2] === "=") {
-            bufferLength--;
-        }
-    }
-
-    var arraybuffer = new ArrayBuffer(bufferLength),
-        bytes = new Uint8Array(arraybuffer);
-
-    for (i = 0; i < len; i += 4) {
-        encoded1 = lookup[base64.charCodeAt(i)];
-        encoded2 = lookup[base64.charCodeAt(i + 1)];
-        encoded3 = lookup[base64.charCodeAt(i + 2)];
-        encoded4 = lookup[base64.charCodeAt(i + 3)];
-
-        bytes[p++] = (encoded1 << 2) | (encoded2 >> 4);
-        bytes[p++] = ((encoded2 & 15) << 4) | (encoded3 >> 2);
-        bytes[p++] = ((encoded3 & 3) << 6) | (encoded4 & 63);
-    }
-
-    return arraybuffer;
+    return Buffer.from([]);
 };
 
 function hexdump(buffer, blockSize) {
@@ -98,6 +70,7 @@ function hexdump(buffer, blockSize) {
 app.post('/getlicense', (req, resp) => {
     console.log("RequestBody", decode(req.body.body));
     let reqBody = decode(req.body.body);
+    // let base64data = reqBody.toString('base64');
     let payload = String.fromCharCode.apply(null, new Uint8Array(reqBody));
     console.log(hexdump(payload,16));
     console.log("Type Of Payload ", typeof payload);
